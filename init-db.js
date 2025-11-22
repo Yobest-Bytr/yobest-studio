895632df76ea6e5fc899d968915c482583895f78
Yobest-Bytr <yobestbytrr@gmail.com>
2025-11-23 00:20:06 +0100

    Delete init-db.js

--- init-db.js
+++
@@ -1,3 +0,0 @@
-import { sql } from '@vercel/postgres';
-await sql`CREATE TABLE IF NOT EXISTS stats (id SERIAL PRIMARY KEY, type TEXT NOT NULL, created_at TIMESTAMP DEFAULT NOW())`;
-console.log("Database initialized");
