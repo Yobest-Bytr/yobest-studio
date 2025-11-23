de4c9c997c31a03605600fd5a13343b682bc5679
Yobest-Bytr <yobestbytrr@gmail.com>
2025-11-23 17:37:11 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import '../styles/globals.css'; // Assuming global styles
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    // Track visitor on every page load (replaces old Firebase onload)
     fetch('/api/visitors', { method: 'POST' })
       .catch(err => console.error('Visitor tracking failed:', err));
   }, []);
